85466b381fe6636f02b3a6bc091bb422ab71642e
莊鈞宇 <jimmyy5121@gmail.com>
2018-10-16 14:28:02 +0800

    Update 066. Plus One.js

--- 066. Plus One.js
+++ 066. Plus One.js
@@ -25,11 +25,11 @@ var plusOne = function(digits) {
     let carry=0;
     for(let i=digits.length-1;i>=0;i--)
     {
-        let sum=0;
+        let sum=carry;
         if(i==digits.length-1)
-            sum=(digits[i]+1);
+            sum+=(digits[i]+1);
         else
-            sum=digits[i];
+            sum+=digits[i];
         carry=parseInt(sum/10);
         digits[i]=sum%10;
     }
